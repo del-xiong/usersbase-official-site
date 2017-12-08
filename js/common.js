8762d3c086c8c3172e7bd0b474b05e2334f8f1d0
Xun <Xun@user.com>
2017-12-08 18:53:30 +0800

    提交接口暂时隐藏

--- js/common.js
+++ js/common.js
@@ -9,16 +9,16 @@ $(document).ready(function () {
 		controlNav: true
 	});
 	//用户信息提交
-	$('#send-submit').click(function (){
-		$.ajax({
-			url: 'www.fsdress.com/index.php?route=checkout/cart/edit',
-			type: 'post',
-			data: $('#first-name,#company,#e-mail,#telephone,#more-content'),
-			dataType: 'html',
-			success: function(html) {
-			}
-		});
-	})
+	// $('#send-submit').click(function (){
+	// 	$.ajax({
+	// 		url: '',
+	// 		type: 'post',
+	// 		data: $('#first-name,#company,#e-mail,#telephone,#more-content'),
+	// 		dataType: 'html',
+	// 		success: function(html) {
+	// 		}
+	// 	});
+	// })
 });
 
 //按钮点击平滑滚动到锚点
